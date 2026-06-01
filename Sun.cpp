824037bf40c560b753f6080ea4262f1abf85797e
Aaadumb <3515006175@qq.com>
2026-06-01 12:13:40 +0800

    Update Sun.cpp

--- Sun.cpp
+++ Sun.cpp
@@ -1,4 +1,5 @@
 ﻿#include "Sun.h"
+
 void Sun::releaseSkill(vector<pair<int, int>>&generateCoin, 
 	vector<pair<int,pair<int, int>>>&generaterbullet, vector<Zombie*>(&v)[6][10],
 	vector<pair<int, int>>& diePlant, vector<pair<pair<int, int>, int>>& dieZombie) {
